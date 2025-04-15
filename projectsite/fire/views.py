de9469e30c56525d513016e997867a710e7d8129
Panda <115606194+Pandasaez@users.noreply.github.com>
2025-04-15 14:28:45 +0800

    Filter

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -24,16 +24,24 @@ def map_station(request):
     return render(request, 'map_station.html', context)  
 
 def map_incident(request):
-    incidents = Locations.objects.values('name', 'latitude', 'longitude')
+    # Get all relevant fields INCLUDING city
+    incidents = Locations.objects.values('name', 'latitude', 'longitude', 'city')
 
     for fs in incidents:
         fs['latitude'] = float(fs['latitude'])
         fs['longitude'] = float(fs['longitude'])
 
+    # Collect unique cities for the dropdown filter
+    cities = Locations.objects.values_list('city', flat=True).distinct()
+
     incident_list = list(incidents)
+    city_list = list(cities)
 
     context = {
         'incident': incident_list,
+        'cities': city_list,  # Pass cities to template
     }
 
     return render(request, 'map_incidents.html', context)
+
+
